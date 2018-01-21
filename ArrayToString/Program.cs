ed4936e491c750ec38437a1238472d14e790d659
Avinash <ashinde@healthgrades.com>
2018-01-21 12:47:59 +0530

    adding project

---
+++ ArrayToString/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayToString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            string[] arr_temp = Console.ReadLine().Split(' ');
+            int[] arr = Array.ConvertAll(arr_temp, Int32.Parse);
+            Array.Reverse(arr);
+            string arrSt = string.Join(" ", arr.Select(p => p.ToString()).ToArray());
+            Console.WriteLine(arrSt);
+            Console.Read();
+        }
+    }
+}
